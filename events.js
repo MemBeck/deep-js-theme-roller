2d72f85a76603808e59b54084b3908b86c71e252
s-a <stephan.ahlf@googlemail.com>
2013-10-02 08:20:32 +0200

    jslint

--- events.js
+++ events.js
@@ -1,10 +1,4 @@
-
 (function() {
-	var dynamicStyleControler = function(selectorText, styleName, styleValue) {
-		return this;
-	};
-
-
 	var dynamicStyles = [];
 	var dynamicStyle = function(selectorText, styleName, styleValue) {
 		this.selectorText = selectorText;
@@ -22,9 +16,7 @@
 			var test = match.match(matchColors);
 			self.r = test[1];
 			self.g = test[2];
-			self.b = test[3];  
-			
-			console.log(self);
+			self.b = test[3];   
 			return styleValue.replace(match, "{0}");
 		});
 		// } catch (e) { 
@@ -50,51 +42,53 @@
 	var getStyle = function(className) {
 
 		var colorRules = [];
-		var processSelector = function(rule) {
+		var processSelector = function(rule){
 
 		 
 			for (var key in rule.style) {
 				var style = rule.style[key];
 				if (typeof (style) === "string" && key !== "cssText" && style.indexOf("rgb") !== -1){
-				 	 
+
 					var test = new dynamicStyle(rule.selectorText, key, style);
 				 
 					test.val();
 					//console.log(rule.selectorText, key, style);
 				}
 			};
-		}
+		};
+
 		var processCss = function(classes) {
 			if(classes){	
-				for(var x=0;x<classes.length;x++) {
+				for(var x=0; x < classes.length ; x++){
 	                // (classes[x].cssText) ? alert(classes[x].cssText) : alert(classes[x].style.cssText);
 	                var selector = classes[x];
 	                if (selector){
-	                	processSelector(selector);
+						processSelector(selector);
 	                }
-	            }	
-	        }
-	    } ;
+	            }
+			}
+	    };
+
 		var styles = document.styleSheets;
 		for (var s = 0; s < styles.length; s++) {
 			var style = styles[s];
 			if (!style.styleSheet){
 				if (style.cssRules)
-			    	for (var i = 0; i < style.cssRules.length; i++) {
-			    		var sheet = style.cssRules[i].styleSheet;
-			    		if (sheet){
-			    			var rules = sheet.rules || sheet.cssRules
-			    			processCss(rules);
-			    		}
-			    	};
+					for (var i = 0; i < style.cssRules.length; i++) {
+						var sheet = style.cssRules[i].styleSheet;
+						if (sheet){
+							var rules = sheet.rules || sheet.cssRules;
+							processCss(rules);
+						}
+					}
 			} else {
-	    		processCss(style);
+				processCss(style);
 			}
 		};
 		console.log(dynamicStyles);
-	}
+	}; 
 
-	Deep.on("sa.theme-roller.index.render", function  () { 
-		getStyle('.container');		
-	})
+	Deep.on("sa.theme-roller.index.render", function(){
+		getStyle(".container");
+	});
 })(jQuery);
